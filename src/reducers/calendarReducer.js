7a464e1950740b04ab746d965861e28b311d0427
RafaelCV <cavr134@gmail.com>
2021-07-05 16:52:06 -0500

    active event logout clear

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -51,11 +51,16 @@ export const calendarReducer = (state = initialState, action) => {
                 ),
                 activeEvent: null
             }
-            case types.eventLoaded:
-                return {
-                    ...state,
-                    events: [...action.payload]
-                }
+        case types.eventLoaded:
+            return {
+                ...state,
+                events: [...action.payload]
+            }
+        case types.eventLogout:
+            return {
+                ...initialState
+            }
+
         default:
             return state;
     }
